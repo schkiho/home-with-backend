ec886b644534a7dad508e3aa7c987a85bb42e3a2
schkiho <schkiho@yahoo.de>
2020-05-05 22:29:58 +0200

    add profile route

---
+++ routes/api/profile.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const request = require('request');
+const config = require('config');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('firstName', 'Firstname is required').not().isEmpty(),
+      check('lastName', 'Lastname is required').not().isEmpty(),
+      check('phone', 'Phonenumber is required').not().isEmpty(),
+      check('salary', 'Salary is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { firstName, lastName, phone, salary } = req.body;
+
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (firstName) profileFields.firstName = firstName;
+    if (lastName) profileFields.lastName = lastName;
+    if (phone) profileFields.phone = phone;
+    if (salary) profileFields.salary = salary;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      // Create
+      profile = new Profile(profileFields);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['email']);
+    res.status(200).json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['email']);
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+    res.status(200).json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
