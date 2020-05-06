c4b90056ecd8faef7f4d19590c03c332100de559
schkiho <schkiho@yahoo.de>
2020-05-06 13:10:34 +0200

    setup routes and redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landing from './components/layout/Landing';
+import SignUp from './components/auth/SignUp';
+
 import './App.scss';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/signup' component={SignUp} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/redux/reducers/authReducer.js
@@ -0,0 +1,7 @@
+import { SIGNUP_SUCCESS, SIGNUP_FAIL } from '../actions/actionTypes';
+
+const initialState = {};
+
+export default function (state = initialState, action) {
+  return state;
+}
